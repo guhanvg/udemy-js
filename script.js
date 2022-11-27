d58374e337c6e8da50b198792494a4d5c5410d5f
guhanvg <guhaneswaran.e@gmail.com>
2022-11-27 13:07:48 +0530

    changes day-2

---
+++ script.js
@@ -0,0 +1,26 @@
+// console.log("hii");
+
+
+// Udemy course partice //
+
+
+//alert("welcome guhan");
+
+// let firstName="guhan";
+// console.log(firstName)
+
+// let country= "india";
+// let continent= "asia";
+// let population="15000000";
+ alert("js is working");
+
+
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+// let ageGuhan= 1998-2031;
+// console.log(ageGuhan);
+
+
+"use strict"
